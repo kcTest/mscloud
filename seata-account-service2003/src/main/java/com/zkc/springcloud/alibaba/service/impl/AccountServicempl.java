25c8c3084aec04061f6694e0e2f31c1b09e8bd8d
pczkc <763336180@qq.com>
2021-11-08 18:58:30 +0800

    seata-order-service mapper 获取返回主键， 不使用事务 正常创建

--- seata-account-service2003/src/main/java/com/zkc/springcloud/alibaba/service/impl/AccountServicempl.java
+++ seata-account-service2003/src/main/java/com/zkc/springcloud/alibaba/service/impl/AccountServicempl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -20,12 +19,12 @@ public class AccountServicempl implements AccountService {
     public void decrease(Long userId, BigDecimal money) {
         log.info("①---->账户扣减开始");
 
-        //模拟超时异常 全局事务回滚
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        //模拟超时异常 全局事务回滚
+//        try {
+//            TimeUnit.SECONDS.sleep(5);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         accountDao.decrease(userId, money);
 
