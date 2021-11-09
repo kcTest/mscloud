f2794b81d5b62d9508d6693af2a2d8d8098968a9
pczkc <763336180@qq.com>
2021-11-09 12:16:50 +0800

    先扣减再模拟超时

--- seata-account-service2003/src/main/java/com/zkc/springcloud/alibaba/service/impl/AccountServicempl.java
+++ seata-account-service2003/src/main/java/com/zkc/springcloud/alibaba/service/impl/AccountServicempl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -18,17 +19,16 @@ public class AccountServicempl implements AccountService {
     @Override
     public void decrease(Long userId, BigDecimal money) {
         log.info("①---->账户扣减开始");
-
-//        //模拟超时异常 全局事务回滚
-//        try {
-//            TimeUnit.SECONDS.sleep(5);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        log.info("①---->账户扣减完成");
 
         accountDao.decrease(userId, money);
 
-        log.info("①---->账户扣减完成");
+        //模拟超时异常 先执行 检查是否全局事务回滚
+        try {
+            TimeUnit.SECONDS.sleep(6);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
